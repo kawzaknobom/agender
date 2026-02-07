57e1fb071db865914e5b7cb625ac3bfba142acb7
kawzaknobom <142733725+kawzaknobom@users.noreply.github.com>
2026-02-07 09:05:41 +0200

    Update SSRNET_model.py

--- age_gender_ssrnet/SSRNET_model.py
+++ age_gender_ssrnet/SSRNET_model.py
@@ -2,16 +2,16 @@ import logging
 import sys
 import numpy as np
 from tf_keras.models import Model
-from tf_keras.layers import Input, Activation, add, Dense, Flatten, Dropout, Multiply, Embedding, Lambda, Add, Concatenate, Activation
-from tf_keras.layers.convolutional import Conv2D, AveragePooling2D, MaxPooling2D
-from tf_keras.layers.normalization import BatchNormalization
+from tf_keras.layers import (
+    Input, Activation, add, Dense, Flatten, Dropout, Multiply, 
+    Embedding, Lambda, Add, Concatenate, Conv2D, AveragePooling2D, 
+    MaxPooling2D, BatchNormalization, Layer
+)
 from tf_keras.regularizers import l2
 from tf_keras import backend as K
-from tf_keras.optimizers import SGD,Adam
+from tf_keras.optimizers import SGD, Adam
 from tf_keras.utils import plot_model
-from tf_keras.engine.topology import Layer
 from tf_keras import activations, initializers, regularizers, constraints
-
 sys.setrecursionlimit(2 ** 20)
 np.random.seed(2 ** 10)
 
